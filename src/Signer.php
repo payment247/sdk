1e70bcccbf8342e21f9f5d34f94f7c2c3741dcf7
payment247 <serhii@247pay.com>
2017-03-20 13:41:03 +0200

    add old signature check

--- src/Signer.php
+++ src/Signer.php
@@ -40,6 +40,19 @@ class Signer
 
         return hash('sha256', $baseString);
     }
+    
+    /**
+     * @param array $data
+     *
+     * @return string
+     * @deprecated
+     */
+    protected function getSignatureOld(array $data)
+    {
+        $baseString = $this->concatenateParams($data) . $this->privateKey;
+    
+        return hash('sha256', $baseString);
+    }
 
     /**
      * @param array $data
@@ -51,6 +64,30 @@ class Signer
         $signature = $data['sign'];
         unset($data['sign']);
 
-        return $this->getSignature($data) === $signature;
+        return $this->getSignature($data) === $signature ||
+            $this->getSignatureOld($data) === $signature;
+    }
+    
+    /**
+     * @param array $data
+     * @param string $arrayKey
+     * @return string
+     */
+    protected function concatenateParams(array $data = [], $arrayKey = '')
+    {
+        ksort($data);
+        
+        $string = '';
+        
+        foreach ($data as $key => $value) {
+            $generatedArrayKey = empty($arrayKey) ? $key : ($arrayKey . '[' . $key . ']');
+            if (is_array($value)) {
+                $string .= $this->concatenateParams($value, $generatedArrayKey);
+            } else {
+                $string .= $generatedArrayKey . '=' . $value;
+            }
+        }
+        
+        return $string;
     }
 }
